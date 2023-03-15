f82f15d2ef3849f7eecb8056d90427d5fa5bdce8
Nicholas Moore <nmoore22@kent.edu>
2023-03-15 18:12:41 +0000

    testing GitHub

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-#main for site
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+#testing for GitHub
+#Hi from nano
